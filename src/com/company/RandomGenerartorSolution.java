a21344f512c2bda2bc68387be7cc4f4b53751394
GBTC440008UR <GBTC440008UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-26 10:25:49 -0400

    Initial Commit

---
+++ src/com/company/RandomGenerartorSolution.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Random;
+
+public class RandomGenerartorSolution {
+    public static void main(String[] args)
+    {
+        System.out.println("Generate 10 random integers between 5 and 95");
+
+        Random rnd = new Random();
+
+        for (int i = 1; i <= 10; ++i)
+        {
+            int randomInt = 5 + rnd.nextInt(91);
+            System.out.println("Generated number: " + randomInt);
+        }
+
+        System.out.println("Done.");
+    }
+}
